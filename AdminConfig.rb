7e707b3f61abe3d94e6e9da35b6380ed515e770d
Hung-Chi Su <atdog.cs96@gmail.com>
2014-06-03 01:13:42 +0800

    change user -> challenger

--- AdminConfig.rb
+++ AdminConfig.rb
@@ -2,6 +2,6 @@ class AdminConfig
     class << self; attr_accessor :db, :challenger, :root_privatekey end
 
     @db = "/ctf/scoreboard/db/scoreboard.sqlite3"
-    @challenger = "user"
+    @challenger = "challenger"
     @root_privatekey = "/home/atdog/admin_script/id_rsa"
 end
