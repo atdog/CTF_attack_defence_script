0685ba39312ad5dddcb10068956f3f27fdf5239a
Hung-Chi Su <atdog.cs96@gmail.com>
2014-06-04 19:58:38 +0800

    change service1 into variable

--- cleanup.rb
+++ cleanup.rb
@@ -11,15 +11,21 @@ def cleanup
     w = Workstation.new(host, "root", AdminConfig.root_privatekey)
 
     begin
-        puts "Remove user: #{challenger}"
         w.exec_remote("id #{challenger}")
+
+        puts "Remove user from sudoers"
+        w.exec_remote("sed -E '/#{challenger}\\sALL/d' -i /etc/sudoers")
+        w.exec_remote("sed -E '/Cmnd_Alias XINETD/d' -i /etc/sudoers")
+
+        puts "Remove user: #{challenger}"
         w.exec_remote("rm -rf /home/#{challenger}")
-        w.exec_remote("deluser --remove-all-files #{challenger}")
+        w.exec_remote("deluser --remove-all-files #{challenger} 2>/dev/null")
         w.exec_local("rm -f ./user_keys/#{user_key}*")
 
-        puts "Remove user: service1"
-        w.exec_remote("id service1")
-        w.exec_remote("deluser --remove-all-files service1")
+        service_name = "service1"
+        puts "Remove user: #{service_name}"
+        w.exec_remote("id #{service_name}")
+        w.exec_remote("deluser --remove-all-files #{service_name} 2>/dev/null")
 
         puts "Remove service"
         w.exec_remote("rm -f /etc/xinetd.d/*")
