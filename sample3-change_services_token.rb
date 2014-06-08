e0f12073665899ab90f772eaa0f37cfbdad9696c
Hung-Chi Su <atdog.cs96@gmail.com>
2014-06-08 23:24:57 +0800

    multithread and update ctfs round id

--- sample3-change_services_token.rb
+++ sample3-change_services_token.rb
@@ -1,7 +1,9 @@
 #!/usr/bin/env ruby
 #
 require_relative 'lib/CTF'
+require_relative 'lib/AdminConfig'
 require 'open3'
+require 'thread/pool'
 
 if __FILE__ == $0
     if ARGV.size != 3
@@ -15,16 +17,34 @@ if __FILE__ == $0
 
     if round <= max_round
         ctf = CTF.new ctf_id
+
+        pool = Thread.pool(10)
         ctf.each_team do |team|
-            puts "[+] Create workstation for #{team['name']}. IP: #{team['ip']}"
-            w = team["workstation"]
-            puts "[+] Max: #{max_round} Round: #{round}"
-            w.change_services_token round
-            # set next round
-            round += 1
-            o, s = Open3.capture2("at now + 5 min", :stdin_data => "/home/atdog/.rvm/rubies/ruby-2.1.2/bin/ruby /home/atdog/admin_script/sample3-change_services_token.rb #{ctf_id} #{max_round} #{round} >> /home/atdog/admin_script/change_token.log 2>&1 ")
-            puts o
+            pool.process team do |arg|
+                puts "[+] Find workstation for #{arg['name']}. IP: #{arg['ip']}"
+                puts "[+] Max: #{max_round} Round: #{round}"
+                arg["workstation"].change_services_token round
+            end
         end
+        pool.shutdown
+        # set next round
+
+        db = SQLite3::Database.open( AdminConfig.db )
+
+        begin
+            services = db.execute("update ctfs set round = :round where id = :ctfid", "round" => round ,"ctfid" => ctf_id)
+        rescue
+            fail "Query services error"
+        end
+
+        o, s = Open3.capture2("cd /ctf/scoreboard/ && /home/atdog/.rvm/rubies/ruby-2.1.2/bin/rake scoring:current")
+        fail "Failed to rake scoring:current" if not s.success?
+        puts o
+
+        # new at
+        round += 1
+        o, s = Open3.capture2("at now + 5 min", :stdin_data => "/home/atdog/.rvm/rubies/ruby-2.1.2/bin/ruby /home/atdog/admin_script/sample3-change_services_token.rb #{ctf_id} #{max_round} #{round} >> /home/atdog/admin_script/change_token.log 2>&1 ")
+        puts o
     else
         puts "Close CTF"
     end
