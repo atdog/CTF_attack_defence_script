a6cf611002059b20d31bf3a995b2a96bf83646ec
Hung-Chi Su <atdog.cs96@gmail.com>
2014-06-14 22:05:07 +0800

    add timeout

--- sample4-service_check.rb
+++ sample4-service_check.rb
@@ -1,6 +1,8 @@
 #!/usr/bin/env ruby
 #
 require_relative 'lib/CTF'
+require 'timeout'
+require 'thread/pool'
 
 if __FILE__ == $0
     if ARGV.size != 2
@@ -11,9 +13,15 @@ if __FILE__ == $0
     round = ARGV[1].to_i
     ctf = CTF.new ARGV[0]
     ctf.each_team do |team|
-        puts "[+] Create workstation for #{team['name']}. IP: #{team['ip']}"
-        w = team["workstation"]
-        puts "Round: #{round}"
-        w.check_services_status round
+        begin
+            Timeout.timeout(5) do 
+                puts "[+] Create workstation for #{team['name']}. IP: #{team['ip']}"
+                w = team["workstation"]
+                puts "Round: #{round}"
+                w.check_services_status round
+            end
+        rescue
+            puts "Check Timeout"
+        end
     end
 end
